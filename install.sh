1aa31aa83622dedba9fedc9530555aab3ad4d884
Carlos Bautista <cbautista@ise.com>
2016-03-14 22:09:45 -0400

    Initial Rails app

---
+++ install.sh
@@ -0,0 +1,23 @@
+# apt-get install sqlite3
+# apt-get install libsqlite3-dev
+
+# apt-get install -y ruby
+
+# apt-get install -y rails
+
+# apt-get install zlib1g-dev
+
+# gem install rails bundler
+
+# gem install jquery-rails
+
+# bundle install
+
+# rake db:migrate
+
+# bundle exec rake test
+
+rails new myapp
+
+rails server --port=3000 --binding=0.0.0.0
+
---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '5cc9a2e82ae6ecef6a0360b53bbd3e82ccc0d6f8e556f130913b9ea923ab82f37ffbc36e9da3f6cd9c84ee60007d7bf3c00d06c78b2315d8a90a2f14c6e648fc'
